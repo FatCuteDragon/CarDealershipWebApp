07cdc99df4a77c53d420151f0ae46bf885466728
vingo <maoobo@163.com>
2016-04-24 15:33:12 +0800

    add listen

--- app.js
+++ app.js
@@ -23,13 +23,38 @@ app.use(function(req, res, next) {
 var port = parseInt(process.argv[2] || config.port);
 var server = http.createServer(app);
 server.listen(port);
+server.on('listening', onListening);
 
 /*
-server.on('connection', (req) => {
-	console.log('server connectioned....');
-});
+server.on('error', onError);
 
-server.on('close', (req) => {
-	console.log('server closed....');
-});
-*/
+//Event listener for HTTP server "error" event.
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+ 
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+ */
+//Event listener for HTTP server "listening" event.
+function onListening() {
+	var addr = server.address();
+	var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+	console.log('Listening on ' + bind);
+}
